ecd5414b541fd3d84d44679a69144ec1109b777f
Soumik Saha <soumiksaha2000.ss@gmail.com>
2023-05-11 23:55:58 +0530

    Adding files

---
+++ Day-8(Function Parameters & Caesar Cipher)/greet.py
@@ -0,0 +1,21 @@
+# def greet():
+#     print("Hello there!")
+#     print("How're you?")
+#     print("Hope you're keeping good")
+    
+# greet()
+
+# def greet(name):
+#     print(f"Hello there! {name}")
+#     print(f"How're you? {name}")
+#     print(f"Hope you're keeping good {name}")
+    
+# greet("Soumik")
+
+def greet_with(name,location):
+    print(f"Hello there! {name}. You are in {location}")
+    print(f"How're you? {name}")
+    print(f"What is it like in {location}?")
+    
+greet_with("Soumik","London")
+greet_with(name="Ashvik",location="Bhagalpur")
