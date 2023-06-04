d352175e0b76504d962cb33a8bf862040d49b862
Soumik Saha <soumiksaha2000.ss@gmail.com>
2023-06-04 12:25:38 +0530

    Adding files

---
+++ Day-24(Open,Read and Write Files)/read-write/file.py
@@ -0,0 +1,13 @@
+# file = open("my_file.txt")
+# contents = file.read()
+# print(contents)
+
+with open("/Users/SOUMIK/OneDrive/Desktop/RoadMap-DA.txt") as file:
+    contents = file.read()
+    print(contents)
+    # file.write('New Text.\n')
+    # file.write('King.\n')
+    # file.write('Duggu and Ashish.\n')
+    # file.write('The StreetMaster.\n')
+    # file.write('New GenZ.\n')
+    
